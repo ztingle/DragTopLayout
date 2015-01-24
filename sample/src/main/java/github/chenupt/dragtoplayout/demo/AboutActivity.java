6b31a3458ae36234938e96602b5ea188aad3805c
chenupt <chenupt@gmail.com>
2015-01-25 01:24:22 +0800

    add license

---
+++ sample/src/main/java/github/chenupt/dragtoplayout/demo/AboutActivity.java
@@ -0,0 +1,18 @@
+package github.chenupt.dragtoplayout.demo;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by chenupt@gmail.com on 2015/1/25.
+ * Description TODO
+ */
+public class AboutActivity extends ActionBarActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.activity_about);
+    }
+}
