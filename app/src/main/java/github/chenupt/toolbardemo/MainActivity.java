5aec730e10fb92ff6b9605166c62f9179b898d15
chenupt <chenupt@gmail.com>
2015-01-24 19:02:39 +0800

    add menu list

--- app/src/main/java/github/chenupt/toolbardemo/MainActivity.java
+++ app/src/main/java/github/chenupt/toolbardemo/MainActivity.java
@@ -1,6 +1,7 @@
 package github.chenupt.toolbardemo;
 
 import android.os.Bundle;
+import android.support.v4.view.PagerTabStrip;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
@@ -9,7 +10,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 
-import java.util.ArrayList;
+import com.google.common.collect.Lists;
+
 import java.util.List;
 
 import de.greenrobot.event.EventBus;
@@ -24,6 +26,7 @@ public class MainActivity extends ActionBarActivity {
     private ModelPagerAdapter adapter;
     private ViewPager viewPager;
     private ImageView imageView;
+    private PagerTabStrip pagerTabStrip;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +37,7 @@ public class MainActivity extends ActionBarActivity {
         viewPager = (ViewPager) findViewById(R.id.view_pager);
         dragLayout = (DragTopLayout) findViewById(R.id.drag_layout);
         imageView = (ImageView) findViewById(R.id.image_view);
+        pagerTabStrip = (PagerTabStrip) findViewById(R.id.pager_tab_strip);
 
         toolbar.setTitle("ToolBar");
         setSupportActionBar(toolbar);
@@ -42,33 +46,26 @@ public class MainActivity extends ActionBarActivity {
         // init DragTopLayout
         DragTopLayout.from(this)
                 .open()
+                .setRefreshRadio(1.4f)
                 .listener(new DragTopLayout.SimplePanelListener() {
-                    @Override
-                    public void onPanelStateChanged(DragTopLayout.PanelState panelState) {
-
-                    }
-
                     @Override
                     public void onSliding(float radio) {
 
                     }
                 }).setup(dragLayout);
 
+        // init pager
         PagerModelManager factory = new PagerModelManager();
-        factory.addCommonFragment(TestListFragment.class, getTitles());
+        factory.addCommonFragment(TestListFragment.class, getTitles(), getTitles());
         adapter = new ModelPagerAdapter(getSupportFragmentManager(), factory);
         viewPager.setAdapter(adapter);
     }
 
     private List<String> getTitles(){
-        List<String> list = new ArrayList<>();
-        list.add("dd");
-        list.add("dd");
-        return list;
+        return Lists.newArrayList("TAB 1", "TAB 2", "TAB 3", "TAB 4");
     }
 
-
-
+    // Handle scroll event from fragments
     public void onEvent(Boolean b){
         dragLayout.setTouchMode(b);
     }
@@ -87,26 +84,23 @@ public class MainActivity extends ActionBarActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_menu_icon) {
             if(imageView.getVisibility() == View.GONE){
                 imageView.setVisibility(View.VISIBLE);
             }else{
                 imageView.setVisibility(View.GONE);
             }
-//            dragLayout.closeMenu(true);
+            return true;
+        } else if(id == R.id.action_toggle){
+            dragLayout.toggleMenu();
             return true;
         }
 
